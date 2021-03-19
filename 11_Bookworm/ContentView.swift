2d365e97a156658a5de5432d29405e3d4fb10082
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-18 21:10:03 -0700

    PushButton struct added to ContentView

--- 11_Bookworm/ContentView.swift
+++ 11_Bookworm/ContentView.swift
@@ -15,6 +15,25 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
+    
+    struct PushButton: View {
+        let title: String
+        @State var isOn: Bool
+        
+        var onColors = [Color.red, Color.yellow]
+        var offColors = [Color(white: 0.6), Color(white: 0.4)]
+        
+        var body: some View {
+            Button(title) {
+                self.isOn.toggle()
+            }
+            .padding()
+            .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .shadow(radius: isOn ? 0 : 5)
+        }
+    }
 
     var body: some View {
         List {
